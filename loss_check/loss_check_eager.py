fe26dcdf073f7c9ecf43b2926846adea3c240da8
alon <alon.lahav2@gmail.com>
2018-04-02 21:54:00 +0300

    .

--- loss_check/loss_check_eager.py
+++ loss_check/loss_check_eager.py
@@ -7,14 +7,14 @@ import tensorflow.contrib.eager as tfe
 
 # TODO:
 # . Share with Ayellet
-# . Add one point at a time
 # . Polygon to points loss
 # . EMD Loss - implement
 
 
 # Parameters
-n_dots = 100
-n_dots_to_train = 100
+n_dots = 40
+n_dots_to_train = 40
+n_dots_to_add = 10
 n_iters = 50
 learning_rate = .4
 radiuses = [1, 2, 4]
@@ -54,11 +54,14 @@ def generate_2d_point_cloud(method, n_dots, rs=[1], show=False):
 # Loss
 def charmer_distance(points, vr_points):
     loss = 0
+    n = 0
     for point in points:
-        loss += tf.reduce_min(tf.reduce_sum((point - vr_points) ** 2, axis=1))
+        n += 1
+        loss += tf.reduce_min((tf.reduce_sum((point - vr_points) ** 2, axis=1)))
     for p in range(vr_points.shape[0]):
-        loss += tf.reduce_min(tf.reduce_sum((vr_points[p] - points) ** 2, axis=1))
-    return loss / 10
+        n += 1
+        loss += tf.reduce_min((tf.reduce_sum((vr_points[p] - points) ** 2, axis=1)))
+    return loss / n
 
 # Gradient calculation
 def grad(points, vr_points):
@@ -75,12 +78,11 @@ if video_output:
     plt.plot(points[:, 0], points[:, 1], 'bo')
     h_last = None
 
-curr_est_points = None
-for _ in range(n_dots_to_train):
+curr_est_points = np.zeros((0, 2))
+while curr_est_points.shape[0] < n_dots_to_train:
     # Variable to train
-    tmp = np.random.uniform(-4, 4, (1, 2)) / 100
-    if type(curr_est_points) is np.ndarray:
-        tmp = np.vstack((curr_est_points, tmp))
+    tmp = np.random.uniform(-4, 4, (n_dots_to_add, 2)) / 100
+    tmp = np.vstack((curr_est_points, tmp))
     vr_points = tfe.Variable(tmp, dtype=tf.float32)
 
     print vr_points.shape
@@ -90,7 +92,7 @@ for _ in range(n_dots_to_train):
         vr_points.assign_sub(dPoints * learning_rate)
         curr_est_points = np.array(vr_points.value())
         if video_output:
-            plt.title(iter)
+            plt.title((curr_est_points.shape[0], iter))
             if h_last != None:
                 h_last.remove()
             h_last, = plt.plot(curr_est_points[:, 0], curr_est_points[:, 1], 'r.')
